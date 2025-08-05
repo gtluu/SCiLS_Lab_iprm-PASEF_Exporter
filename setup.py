8ee05a74a31f7e885bf23d6687c5f329e2474580
gtluu <46392631+gtluu@users.noreply.github.com>
2025-08-05 13:26:01 -0500

    Update url and contact

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='iprm-PASEF_Exporter',
       version='0.1.0',
       description='iprm-PASEF MS/MS Export from SCiLS Lab Datasets.',
-      url='',
+      url='https://github.com/gtluu/SCiLS_Lab_iprm-PASEF_Exporter',
       author='Gordon T. Luu',
-      author_email='Gordon.Luu@Bruker.com',
+      author_email='gtluu912@gmail.com',
       license='GPL-2.0',
       packages=['exporter'],
       entry_points={'console_scripts': ['get_feature_lists=exporter.get_feature_list_ids:main',
@@ -13,3 +13,4 @@ setup(name='iprm-PASEF_Exporter',
                                         'iprmpasef_to_mgf=exporter.mgf:main',
                                         'iprmpasef_to_mzml=exporter.mzml:main']},
       install_requires=['numpy', 'pandas', 'pyopenms', 'pyteomics', 'psims', 'PySide6'])
+
